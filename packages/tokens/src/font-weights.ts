010b6c9c8ceb57b9a10f554e034a84b2d241b8e5
Bruno <brn.milano@hotmail.com>
2024-08-12 11:51:53 -0300

    A base de um Design System: - Configurando tokens; - Build do pacote com TSUP; - Configurando Monorepo; - Configuração do TypeScript; - Configuração do ESLint; - Configurando pacote do React;

---
+++ packages/tokens/src/font-weights.ts
@@ -0,0 +1,13 @@
+/**
+ * Font weights
+ *
+ * @type {Object}
+ * @property {string} regular - 400
+ * @property {string} medium - 500
+ * @property {string} bold - 700
+ */
+export const fontWeights = {
+  regular: '400',
+  medium: '500',
+  bold: '700',
+}
