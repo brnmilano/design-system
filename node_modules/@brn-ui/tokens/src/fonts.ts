d120501c92eb287b27eda6d1ba48c5c60612cad7
Bruno <brn.milano@hotmail.com>
2024-08-12 11:51:42 -0300

    A base de um Design System: - Configurando tokens; - Build do pacote com TSUP; - Configurando Monorepo; - Configuração do TypeScript; - Configuração do ESLint; - Configurando pacote do React;

---
+++ node_modules/@brn-ui/tokens/src/fonts.ts
@@ -0,0 +1,11 @@
+/**
+ * @name Font Families
+ *
+ * @type {Object}
+ * @property {string} default - Roboto, sans-serif
+ * @property {string} code - monospace
+ */
+export const fonts = {
+  default: 'Roboto, sans-serif',
+  code: 'monospace',
+}
