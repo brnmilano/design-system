c5c2f7e9ac0a8abf9f89bcabb718b52a0aebf23b
Bruno <brn.milano@hotmail.com>
2024-08-12 15:47:33 -0300

    Design System em React: - Configurando o Stitches; - Criando app em Storybook; - Adicionando fonte externa; - Documentações em cores;

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,31 @@
-import { colors } from '@brn-ui/tokens'
+import { ComponentProps } from 'react'
 
-console.log(colors)
+import { styled } from './styles'
 
-export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
